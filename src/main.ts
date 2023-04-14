a0c5bd8d10148002824e007ae8ae951721df9acf
Franco <franco.torres1705@gmail.com>
2023-04-14 01:08:52 -0300

    cambios cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ async function bootstrap() {
 
 
   const app = await NestFactory.create(AppModule);
-
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://banko-de-sofka.web.app');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
@@ -15,6 +14,12 @@ async function bootstrap() {
   });
   app.enableCors({
     origin: 'https://banko-de-sofka.web.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true
+  });
+
+  app.enableCors({
+    origin: 'http://localhost:4200',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true
   });
