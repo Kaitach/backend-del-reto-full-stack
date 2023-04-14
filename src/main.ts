6bb6742e574f32728765e44c3c0571bb09ba1e9f
Franco <franco.torres1705@gmail.com>
2023-04-14 01:17:54 -0300

    cambiando cors...

--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,18 @@ async function bootstrap() {
 
 
   const app = await NestFactory.create(AppModule);
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://banko-de-sofka.web.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    next();
-  });
-  app.enableCors({
-    origin: 'https://banko-de-sofka.web.app',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true
-  });
+  // app.use((req, res, next) => {
+  //   res.header('Access-Control-Allow-Origin', 'https://banko-de-sofka.web.app');
+  //   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  //   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  //   res.header('Access-Control-Allow-Credentials', 'true');
+  //   next();
+  // });
+  // app.enableCors({
+  //   origin: 'https://banko-de-sofka.web.app',
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //   credentials: true
+  // });
 
   app.enableCors({
     origin: 'http://localhost:4200',
